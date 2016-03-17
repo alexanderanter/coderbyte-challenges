e20323decb670e4ce8e5d7781efe445e2078754b
alexanderanter <alexanderanter@gmail.com>
2016-03-17 12:19:06 +0100

    added simple template for very simple exercises

---
+++ src/ch-2/template/app.js
@@ -0,0 +1,7 @@
+// run tests with 'npm test'
+var simpleFunc= require("./simple-function");
+
+// Test the sum-function here
+
+// Print the result to the terminal (console)
+loopy.triangleLoop();
---
+++ src/ch-2/template/simple-function.js
@@ -0,0 +1,13 @@
+/**
+ * Module for looping a triangle
+ *
+ * @author alexanderanter
+ * @version 1.0.0
+ */
+
+"use strict";
+
+
+exports.simpleFunction = function() {
+
+};
