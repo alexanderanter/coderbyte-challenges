5b6dc53c27677fcf82ff7422669fc2783a36f5e5
Alexander Anter <alexanderanter@gmail.com>
2016-03-26 18:22:10 +0100

    solved more array challenges

--- src/basic-js/arrays/arrays.js
+++ src/basic-js/arrays/arrays.js
@@ -12,10 +12,37 @@ exports.vowelCount = function(data) {
   var vowels = ['a','e','i','o','u'];
   var dataArr = data.split('');
   var i = 0;
+
   for (i; i < dataArr.length; i++) {
       if(vowels.indexOf(dataArr[i]) !== -1) {
         result += 1;
       }
   }
+
   return result;
+};
+
+exports.longestWord = function(data) {
+    var result = "";
+  	var arr = data.split(/[^a-zA-Z0-9.]/);
+  	var len = arr.length;
+  	var i;
+
+  	for (i = 0; i < len; i++){
+
+      	if (i === 0) {
+        	result = arr[i];
+        }else {
+        	if(arr[i].length > result.length) {
+            	result = arr[i];
+            }
+        }
+    }
+  return result;
+};
+
+exports.alphabetSoup = function(data) {
+  var arr = data.split("");
+  arr.sort();
+  return arr.join("");
 };
--- test/basic-js/arrays/arrays.js
+++ test/basic-js/arrays/arrays.js
@@ -8,4 +8,12 @@ describe("Test the module with kind values", function() {
         expect(arrays.vowelCount("hello")).eql(2);
         done();
     });
+    it("Should return paragraph", function(done) {
+        expect(arrays.longestWord("fun&!! time")).eql("time");
+        done();
+    });
+    it("Should return abcde", function(done) {
+        expect(arrays.alphabetSoup("acbde")).eql("abcde");
+        done();
+    });
 });
