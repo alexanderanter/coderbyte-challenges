9704556d369535ed4da696755cee51c3e1283004
alexanderanter <alexanderanter@gmail.com>
2016-03-30 16:04:49 +0200

    fixing the divisionStringified challenge

--- src/easy-js/division-stringified/app.js
+++ src/easy-js/division-stringified/app.js
@@ -1,1 +1,13 @@
-var secondGreatlow = require("./second-greatlow");
+var divString = require("./division-stringified");
+
+var result = divString.divisionStringified(4, 3);
+console.log(result);
+
+var result2 = divString.divisionStringified(503394930, 43);
+console.log(result2 + "  should equal " + "11,706,859");
+
+// var result3 = divString.divisionStringified(101077282, 21123);
+// console.log(result3 + "  should equal " + "4,785");
+//
+// var result4 = divString.divisionStringified(123456789, 10000);
+// console.log(result4 + "  should equal " + "12,346");
--- test/easy-js/division-stringified/division-stringified.js
+++ test/easy-js/division-stringified/division-stringified.js
@@ -6,17 +6,20 @@ var expect = require("chai").expect;
 describe("Test the module with kind values", function() {
 
     it("2,3 should return 1", function(done) {
-        expect(divisionStringified.divisionStringified(2,3)).eql("1");
+        expect(divisionStringified.divisionStringified(2, 3)).eql("1");
         done();
     });
+
     it("123456789, 10000 should return 12,346", function(done) {
         expect(divisionStringified.divisionStringified(123456789, 10000)).eql("12,346");
         done();
     });
+
     it("5, 2 should return 3", function(done) {
         expect(divisionStringified.divisionStringified(5, 2)).eql("3");
         done();
     });
+
     it("6874, 67 should return 103", function(done) {
         expect(divisionStringified.divisionStringified(6874, 67)).eql("103");
         done();
@@ -26,10 +29,12 @@ describe("Test the module with kind values", function() {
         expect(divisionStringified.divisionStringified(1, 10)).eql("0");
         done();
     });
+
     it("503394930, 43 should return 11,706,859", function(done) {
         expect(divisionStringified.divisionStringified(503394930, 43)).eql("11,706,859");
         done();
     });
+
     it("5, 54 should return 0", function(done) {
         expect(divisionStringified.divisionStringified(5, 54)).eql("0");
         done();
@@ -49,7 +54,5 @@ describe("Test the module with kind values", function() {
         expect(divisionStringified.divisionStringified(101077282, 21123)).eql("4,785");
         done();
     });
-
-
 
 });
